7e9bd619c3fab5284e59ebe18c09a44a3b691255
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-12-11 17:42:29 +0000

    Create product.py

---
+++ product.py
@@ -0,0 +1,27 @@
+'''
+Author: Ms Rya
+Version: 3.10
+'''
+from flask import *
+
+app = Flask(__name__)
+
+# Route for the product enrollment form
+@app.route('/')
+def enrollment_form():
+    return render_template('enrollment.html')
+
+@app.route('/submit', methods=['POST'])
+def submit_form():
+    name = request.form['name']
+    code = request.form['code']
+    cost = float(request.form['cost'])
+    quantity = int(request.form['quantity'])
+
+    total = cost * quantity
+    tax = total * 0.18
+    gross_pay = total + tax
+   return render_template('display.html', name=name, code=code, cost=cost, quantity=quantity, total=total, tax=tax, gross_pay=gross_pay)
+
+if __name__ == '__main__':
+    app.run(debug=True)
